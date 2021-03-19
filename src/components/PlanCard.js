f8c48456ea8ddf0b1604a5ed9d02b40a92524b9c
Brian Emilius <be@rts.dk>
2021-03-19 10:39:43 +0100

    add cta event action for analytics

--- src/components/PlanCard.js
+++ src/components/PlanCard.js
@@ -17,7 +17,7 @@ function PlanCard({ heading, summary, price, link, features, getfrom }) {
 				? <><p className="features__getfrom">Get all {getfrom} features</p><h2 className="features__heading">Plus</h2></>
 				: <h2 className="features__heading">Features</h2> }
 				<ul className="features__list">
-					{features.map(feature => <li className="list__item">{feature}</li>)}
+					{features.map(feature => <li key={feature} className="list__item">{feature}</li>)}
 				</ul>
 			</section>
 		</article>
---
+++ src/helpers/set-cookies.js
@@ -0,0 +1,10 @@
+// src/helpers/set-cookies.js
+
+function setCookies(config) {
+	if (config.acceptanalytics) {
+		// eslint-disable-next-line
+		gtag('config', 'G-WT742Y2XSW');
+	}
+}
+
+export default setCookies;
